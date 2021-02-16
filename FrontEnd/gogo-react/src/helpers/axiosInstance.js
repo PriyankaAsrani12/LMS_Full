a989ba628b52eee455b31c46e1b50b50726beb0d
hari-44456 <naraharipapshetwar@gmail.com>
2021-02-16 17:11:45 +0700

    removed port no and added o auth id

--- FrontEnd/gogo-react/src/helpers/axiosInstance.js
+++ FrontEnd/gogo-react/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Instance = axios.create({
-   baseURL: `${window.location.protocol}//${window.location.hostname}:5000`,
+  baseURL: `${window.location.protocol}//${window.location.hostname}`,
   credentials: 'include',
   withCredentials: true,
 });
