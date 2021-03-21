890e4318ff08f64dea6b79511fd1c2a1c5b87fb3
Ubuntu <ubuntu@ip-172-31-33-45.ap-south-1.compute.internal>
2021-03-21 03:07:23 +0000

    dfdf

--- BackEnd/test.js
+++ BackEnd/test.js
@@ -1,6 +1,6 @@
 const cmd =require("node-cmd");
 var file = "jitul.txt";
-var asd = "bnycdn cp -s zone1 ./upload/"+file+" ./teststoragezone123/upload/"+file
+var asd = "bnycdn cp -s oye-jitul-57 ./upload/"+file+" ./oye-jitul-57/upload/"+file
 const com= cmd.runSync(asd);
 
 console.log(asd);
