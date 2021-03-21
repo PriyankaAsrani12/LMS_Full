c03e4a01166f629a633b2c6a6b24f02394f9815c
Jitul Teron <jitulteron9@gmail.com>
2021-03-21 09:38:23 +0530

    extensio added

--- BackEnd/TempFile/temp.js
+++ BackEnd/TempFile/temp.js
@@ -1,4 +1,7 @@
-const cmd =require("node-cmd");
 
-const com= cmd.runSync('bnycdn cp -s zone1 -R ./upload/ ./teststoragezone123/upload/');
-console.log(com);
+    const path=require("path");
+    let file="Jitul.html";
+    let nameis=file.split('.').slice(0, -1).join('.');
+    let extension=file.split('.').slice
+    let newname=`${nameis}-${Date.now()}`;
+    console.log(path.parse(file).ext);
