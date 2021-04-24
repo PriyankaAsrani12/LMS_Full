9aca2b3f4e49977b834199fdc0ac30f159ece2c8
PriyankaAsrani12 <priyankaasrani01234@gmail.com>
2021-04-24 10:28:34 +0530

    Update index.js

--- BackEnd/Student/index.js
+++ BackEnd/Student/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { User } = require('../Tutor/loginSignup/customer/models');
 
 router.use('/auth', require('./loginSignUp'));
 router.use('/sessions', require('./sessions'));
@@ -9,6 +10,29 @@ router.use('/mycourses', require('./mycourses'));
 router.use('/cart', require('./cart'));
 router.use('/affiliates', require('./affiliate'));
 router.use("/info",require("./loginSignUp/info"));
-router.use("/comment",require("./Comment"))
+router.use("/comment",require("./Comment"));
+
+
+router.get('/clientDetails/:id',async(req,res)=>{
+    customer_id=req.params.id
+  
+    sqlCheck = await User.findOne({
+      where: {
+        customer_id,
+      },
+      attributes:['customer_institute_name','customer_institute_logo_url','customer_institute_favicon_icon_url']
+    });
+  
+    if (!sqlCheck) {
+      return res.status(404).json({
+        success: 0,
+        error: 'Client not registered',
+      });
+    }else{
+      return res.json(sqlCheck.dataValues)
+    }
+})
+
+
 module.exports = router;
 // /student/auth/login
